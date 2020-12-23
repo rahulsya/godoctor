65a11c812b29b814a57528b2c9e068df097a8fb5
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-12-23 23:31:30 +0700

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
 import { StyleSheet } from 'react-native'
-import { GetStarted } from './pages'
+// import { GetStarted } from './pages'
+import Router from './router'
 
 export default function App() {
   return (
     // <Splash/>
-    <GetStarted/>
+    <NavigationContainer>
+      <Router/>
+    </NavigationContainer>
   )
 }
 // alt+shift+o
