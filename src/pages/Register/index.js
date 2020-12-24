00e6bf4c127284f17e9e45cb3a3f208e69f9a257
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-12-24 23:10:11 +0700

    modified pages login and register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Gap, Header, Input } from '../../components'
+import { colors } from '../../utils/colors'
 
-export default function Register() {
+export default function Register({navigation}) {
     return (
-        <View>
-            <Text>This Register</Text>
+        <View style={styles.container}> 
+            <Header onPress={()=>navigation.goBack()} title="Daftar Akun"/>
+            <View style={styles.content}>
+                <Input label="Full Name"/>
+                <Gap height={24}/>
+                <Input label="Pekerjaan"/>
+                <Gap height={24}/>
+                <Input label="Email Address"/>
+                <Gap height={24}/>
+                <Input label="Password"/>
+                <Gap height={40}/>
+                <Button title="Continue" type="primary"/>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    content:{
+        backgroundColor:'white',
+        padding:40,
+        paddingTop:0,
+        // justifyContent:'space-between'
+    },
+    container:{
+        backgroundColor:colors.white,
+        flex:1,
+    }
+})
