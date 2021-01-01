1d51cb039c92674a488a2438a9bf4f0b604fa40d
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-01-01 22:22:44 +0700

    register bottom tabs

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import { GetStarted, Register, Splash,Login } from '../pages'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { GetStarted, Register, Splash,Login,UploadPhoto,Doctor,Hospitals,Messages } from '../pages'
+import { BottomNavigator } from '../components'
 
 const Stack=createStackNavigator()
+const Tab=createBottomTabNavigator()
+
+const MainApp=()=>{
+    return(
+        <Tab.Navigator tabBar={props=><BottomNavigator {...props}/>}>
+            <Tab.Screen name="Doctor" component={Doctor}/>
+            <Tab.Screen name="Messages" component={Hospitals}/>
+            <Tab.Screen name="Hospitals" component={Messages}/>
+        </Tab.Navigator>
+    )
+}
 
 const Router=()=>{
     return(
-        <Stack.Navigator initialRouteName="Splash">
+        <Stack.Navigator initialRouteName="MainApp">
             <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
             <Stack.Screen name="GetStarted" component={GetStarted} options={{headerShown:false}}/>
             <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
             <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
+            <Stack.Screen name="UploadPhoto" component={UploadPhoto} options={{headerShown:false}}/>
+            <Stack.Screen name="MainApp" component={MainApp} options={{headerShown:false}}/>
         </Stack.Navigator>
     )
 }
