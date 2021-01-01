3820456f9b0e86e3bfed1cd5d09ab1e04cace848
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-01-01 22:23:01 +0700

    add new color

--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -1,5 +1,6 @@
 const mainColors={
     dark:'#112340',
+    dark2:'#495A75',
     gray:'#7D8797',
     gray2:'#E9E9E9',
     blue:'#0BCAD4',
@@ -12,9 +13,12 @@ export const colors={
     secondary:'white',
     white:'white',
     black:'black',
+    dark:mainColors.dark,
     text:{
         default:mainColors.dark,
         secondary:mainColors.gray,
+        menuActive:mainColors.blue,
+        menuInActive:mainColors.dark2,
     },
     button:{
         primary:{
