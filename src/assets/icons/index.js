7d15d8f01620a5847cf8c2fc1dedab11a56c61d0
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-01-01 22:20:14 +0700

    add new icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,21 @@
 import IconBackDark from './arrow_back.svg'
+import ICBtnPlus from './Ic_btn_plus.svg'
+import ICBtnRemove from './Ic_btn_remove.svg'
+import ICDoctor from './ic-doctor.svg'
+import ICDoctorActive from './ic-doctor-active.svg'
+import ICHospitalActive from './ic-hospitals-active.svg'
+import ICHospital from './ic-hospitals.svg'
+import ICMessagesActive from './ic-messages-active.svg'
+import ICMessages from './ic-messages.svg'
 
 export{
-    IconBackDark
+    IconBackDark,
+    ICBtnPlus,
+    ICBtnRemove,
+    ICDoctor,
+    ICDoctorActive,
+    ICHospitalActive,
+    ICHospital,
+    ICMessagesActive,
+    ICMessages,
 }
--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,7 +1,9 @@
 import ILLogo from './logo.svg'
 import ILGetStarted from './get-started.png'
+import ILNullPhoto from './Photo-null.png'
 
 export{
     ILLogo,
-    ILGetStarted
+    ILGetStarted,
+    ILNullPhoto
 }
